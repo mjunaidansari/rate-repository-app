9a9e5067a91227df5ff6cd01bd70bbf24bee1203
Junaid Ansari <junaidshahid.ansari@gmail.com>
2023-10-08 10:10:14 +0530

    learning styling components

--- App.js
+++ App.js
@@ -1,15 +1,61 @@
-import { View } from "react-native"
+// import { View } from "react-native"
 
-import Main from "./src/components/Main"
-import RepositoryList from "./src/components/RepositoryList"
+// import Main from "./src/components/Main"
+// import RepositoryList from "./src/components/RepositoryList"
+
+// const App = () => {
+// 	return (
+// 		<View>
+// 			<Main/>
+// 			<RepositoryList/>
+// 		</View>
+// 	)
+// }
+
+// export default App
+
+import { Text, StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+
+	text: {
+		color: 'grey',
+		fontSize: 14,
+	},
+	blueText: {
+		color: 'blue'
+	},
+	bigText: {
+		fontSize: 24,
+		fontWeight: '700',
+	}
+
+})
+
+const FancyText = ({ isBlue, isBig, children }) => {
+	
+	const textStyles = [
+		styles.text,
+		isBlue && styles.blueText,
+		isBig && styles.bigText
+	]
+
+	return <Text style={textStyles}>{children}</Text>
+}
 
 const App = () => {
+
 	return (
-		<View>
-			<Main/>
-			<RepositoryList/>
-		</View>
+		<>
+			<FancyText>Simple Text</FancyText>
+			<FancyText isBlue>Blue Text</FancyText>
+			<FancyText isBig>Big Text</FancyText>
+			<FancyText isBig isBlue>
+				Big Blue Text
+			</FancyText>
+		</>
 	)
+
 }
 
 export default App
